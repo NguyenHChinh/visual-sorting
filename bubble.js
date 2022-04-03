3692e5f1953e6bf292b194af5d7a54a29a6ca6a6
Chinh Nguyen <nguyenchuu@yahoo>
2022-04-02 20:43:05 -0500

    Added shuffle and sort buttons, added bubble sort function

--- bubble.js
+++ bubble.js
@@ -40,17 +40,58 @@ function displayArray(array, current) {
     for (let i = 0; i < array.length; i++) {
         ctx.beginPath();
         ctx.moveTo(
-            (500 / array.length) * i,
+            (500 / array.length) * i + 10,
             500
         );
         ctx.lineTo(
-            (500 / array.length) * i,
+            (500 / array.length) * i + 10,
             500 - (500 / array.length) * integerArray[i]
         );
         ctx.stroke();
     }
 }
 
+function bubbleSort(array) {
+    // Iterate through the array
+    for (let i = 0; i < array.length; i++) {
+        // Move through array from index 0 to last unsorted index
+        for (let j = 0; j < array.length - (i + 1); j++) {
+            // Comparing current value with next value, if current value is GREATER, swap two values
+            if (array[j] > array[j+1]) {
+                let temp = array[j+1];
+                array[j+1] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+    console.log("success");
+
+    return array;
+}
+
+// Creating default array (size 100)
 generateArray(100);
 integerArray = generateArray(100);
 displayArray(integerArray, -2);
+
+// Creating shuffle button
+let shuffleButton = document.createElement("button");
+shuffleButton.innerHTML = "Shuffle";
+shuffleButton.id = "shuffleBtn"
+shuffleButton.className = "button"
+shuffleButton.addEventListener("click", function() {
+    shuffle(integerArray);
+    displayArray(integerArray);
+});
+document.body.appendChild(shuffleButton);
+
+// Creating sort button
+let sortButton = document.createElement("button");
+sortButton.innerHTML = "Sort";
+sortButton.id = "sortBtn"
+sortButton.className = "button"
+sortButton.addEventListener("click", function() {
+    integerArray = bubbleSort(integerArray);
+    displayArray(integerArray)
+});
+document.body.appendChild(sortButton);
