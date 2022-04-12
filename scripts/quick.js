36d9a107434c337593d080fc0313308a257ab2fe
Chinh Nguyen <36144798+NguyenHChinh@users.noreply.github.com>
2022-04-11 23:52:23 -0500

    Starting Implementation of Quick/Merge
    Added array display function and defining async functions for quick and merge sort.
    
    Disabled access to quick and merge sort pages from navigation bar of home/bubble/selection/insertion html files.

--- scripts/quick.js
+++ scripts/quick.js
@@ -2,9 +2,33 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
-function quickSort(array) {
-
+async function quickSort(array) {
+    displayArray(array);
 }
+
+function displayArray(array) {
+    // Clearing canvas to draw updated image
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Stroke size
+    strokeWidth = 1.4 * (Math.pow(0.982, document.getElementById("dataRange").value - 150)) - 0.5;
+    ctx.lineWidth = 1.4 * (Math.pow(0.982, document.getElementById("dataRange").value - 150)) - 0.5;
+
+    for (let i = 0; i < array.length; i++) {
+        ctx.beginPath();
+        ctx.moveTo(
+            (500 / array.length) * i + 15 - (strokeWidth / 2),
+            500
+        );
+        ctx.lineTo(
+            (500 / array.length) * i + 15 - (strokeWidth / 2),
+            500 - (500 / array.length) * array[i]
+        );
+
+        ctx.stroke();
+    }
+}
+
 
 // Creating default array (size 50)
 integerArray = generateArray(25);
